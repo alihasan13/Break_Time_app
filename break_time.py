9fbbe633223d946377b5d09578f707f7d12dc0d4
Ali Hasan <Ali.hasan@northsouth.edu>
2018-09-19 05:20:54 +0600

    changed time duration.

--- break_time.py
+++ break_time.py
@@ -2,9 +2,9 @@ import time
 import webbrowser
 
 break_time=0
-print("current time is"+time.ctime())
+print("current time is "+time.ctime())
 while(break_time<3):
-    time.sleep(5)
+    time.sleep(120)
     webbrowser.open("https://www.youtube.com/watch?v=peyU0EOTiy0")
     break_time=break_time+1
 
